260f0edcac67537086efe5b7c9b55c1b1cfd9019
Guilherme Revoredo <37821225+Guilherme11gr@users.noreply.github.com>
2019-02-09 18:23:42 -0200

    Update UserRepos.js

--- github-info/src/components/UserRepos.js
+++ github-info/src/components/UserRepos.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'; // Stateless component
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class UserRepos extends Component {
